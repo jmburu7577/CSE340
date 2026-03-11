bb0c410fd6bc20caac545f7d671251b230dc4c03
James Mburu Maina <jmaina@knh.or.ke>
2026-03-11 10:02:03 +0300

    fixed the HTML validation errors and added a proper EJS layout

--- CSE Motors/app.js
+++ CSE Motors/app.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
 const PORT = process.env.PORT || 5500;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.set('layout', 'layout');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
